130157697999a96cd501e8e4e7cc421e6b621eb1
viet <viet1282002.com>
2023-11-16 21:49:25 +0700

    enity

---
+++ src/main/java/com/example/www_lab_7/backend/pks/OrderDetailPK.java
@@ -0,0 +1,14 @@
+package com.example.www_lab_7.backend.pks;
+
+import lombok.Getter;
+import lombok.Setter;
+import vn.edu.iuh.fit.backend.models.Order;
+import vn.edu.iuh.fit.backend.models.Product;
+
+import java.io.Serializable;
+
+@Setter @Getter
+public class OrderDetailPK implements Serializable {
+    private Order order;
+    private Product product;
+}
---
+++ src/test/java/com/example/www_lab_7/WwwLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.www_lab_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwLab7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
